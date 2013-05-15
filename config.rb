d914003bd0e8cd1fb05d1a6b162905388ece36b5
Gustavo Krause <emaildokrause@gmail.com>
2013-05-15 11:54:14 -0300

    1º pacote framework fed
    1º pacote de arquivos do framework fed.

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "../"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "imagens"
+javascripts_dir = "js"
+fonts_dir = css_dir + "/fonts"
+
+output_style = :compact
+#output_style = :compressed
