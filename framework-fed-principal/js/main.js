e9402b4ee171a658d5fffb3b67ab0faed8667d65
Gustavo Krause <emaildokrause@gmail.com>
2013-12-13 14:15:03 -0200

    remocao swf v2
    remocao swf versao atual do framework

--- framework-fed-principal/js/main.js
+++ framework-fed-principal/js/main.js
@@ -4,9 +4,6 @@
   --------------
 */
 
-  // Executando SWFObject
-  // swfobject.embedSWF("swf/meu_flash.swf", "conteudo_flash", "300", "120", "9.0.0", "swf/expressInstall.swf", {}, {wmode:"transparent"});
-
 
 $(document).ready(function(){
 
