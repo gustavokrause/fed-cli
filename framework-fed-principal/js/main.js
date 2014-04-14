427a1a07a3e75170b7233a726b056eb7bd93a43f
Gustavo Krause <emaildokrause@gmail.com>
2014-04-14 15:31:33 -0300

    ajustes v5.1
    + mfp
    - abrepopup

--- framework-fed-principal/js/main.js
+++ framework-fed-principal/js/main.js
@@ -5,6 +5,26 @@
 */
 
 
+// abrePopup('#sua-popup');
+function abrePopup(elemento) {
+  $.magnificPopup.open({
+      items: {
+        src: elemento
+      },
+      tClose: 'Fechar Popup',
+      closeMarkup: '<button title="%title%" class="mfp-close">fechar</button>',
+      midClick: true,
+      type: 'inline'
+  });
+}
+
+  // abre popups a partir do rel=subsection e href
+  $('[rel="subsection"]').on('click', function () {
+    abrePopup($(this).attr('href'));
+    return false;
+  });
+
+
 $(document).ready(function(){
 
 
@@ -15,13 +35,13 @@ $(document).ready(function(){
 */
 
 
-  // galeria
-  if($(this).has('ul.galeria').length > 0) {
-    $('ul.galeria').bxSlider();
-  }
 
 });
 
 
-// $(window).load(function(){
-// });
+$(window).load(function(){
+
+  // monta galeria apos carregar a pagina
+  $('ul.galeria').bxSlider();
+
+});
